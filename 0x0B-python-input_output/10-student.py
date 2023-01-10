3147a6ee2fece0bcab6054f08a045a1c374d83a2
DIKKDON <dicksonchideraudeh@gmail.com>
2023-01-10 12:56:51 -0800

    pyhonr input output

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,31 +1,19 @@
 #!/usr/bin/python3
-"""This module contains Student
-that defines a student by:
-"""
+"""students module"""
 
 
 class Student:
-    """This class defines a student"""
+    """Student class"""
     def __init__(self, first_name, last_name, age):
-        """Arguments:
-            firs_name - first name of the student
-            last_name - last name of the student
-            age - age of the student
-        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """
-            retrieves a dictionary representation of a Student instance
-        """
+        if attrs is None:
+            return self.__dict__
         new_dict = {}
-        if type(attrs) is list:
-            if all(type(i) is str for i in attrs):
-                for i in attrs:
-                    if hasattr(self, i):
-                        new_dict[i] = getattr(self, i)
-                return new_dict
-
-        return self.
+        for key in attrs:
+            if key in self.__dict__:
+                new_dict[key] = self.__dict__[key]
+        return new_dict
