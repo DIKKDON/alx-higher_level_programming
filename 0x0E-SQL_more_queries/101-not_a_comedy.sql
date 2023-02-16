b8064451830c8f83a24ba249ee72b5724c66f655
DIKKDON <dicksonchideraudeh@gmail.com>
2023-02-16 00:07:45 -0800

    new update

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -10,8 +10,3 @@ LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
 WHERE tv_genres.name = 'Comedy')
 GROUP BY title
 ORDER BY title ASC;
-Footer
-© 2023 GitHub, Inc.
-Footer navigation
-Terms
-
